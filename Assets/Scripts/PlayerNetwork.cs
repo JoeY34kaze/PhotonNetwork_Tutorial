7e5098243cb09be493dca2ef34860046e4e143b6
FGG <firstgeargames@gmail.com>
2017-04-30 11:31:13 -0600

    - Part 02

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+    public static PlayerNetwork Instance;
+    public string PlayerName { get; private set; }
+
+
+	// Use this for initialization
+	private void Awake ()
+    {
+        Instance = this;
+
+        PlayerName = "Distul#" + Random.Range(1000, 9999);
+	}
+	
+}
